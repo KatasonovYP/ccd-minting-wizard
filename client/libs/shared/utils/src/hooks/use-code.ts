fda42f4664e57c4f4d1a0a4168d85467499a72f0
Yury Katasonov <katasonov-yury@yandex.ru>
2024-04-30 10:16:36 +0300

    add console.error

--- client/libs/shared/utils/src/hooks/use-code.ts
+++ client/libs/shared/utils/src/hooks/use-code.ts
@@ -1,7 +1,6 @@
 import { useState } from 'react';
 import { useContractFeaturesCode } from './use-contract-features-code';
 
-const SMART_CONTRACT_PATH = './processed';
 
 export function useCode() {
     const contractFeaturesCode = useContractFeaturesCode();
@@ -10,23 +9,22 @@ export function useCode() {
     const [reference, setReference] = useState<string>();
     const [schema, setSchema] = useState<string>();
 
-    import(`./processed/${contractFeaturesCode}/src/lib.rs`).then(
-        (lib) => {
-            setCode(lib.plainText.trim());
-        },
-    );
+    import(`./processed/${contractFeaturesCode}/src/lib.rs`)
+        .then((lib) => setCode(lib.plainText.trim()))
+        .catch(console.error);
 
-    import(`./processed/${contractFeaturesCode}/reference.text`).then(
-        (lib) => {
-            setReference(lib.plainText.trim());
-        },
-    );
+    import(`./processed/${contractFeaturesCode}/reference.text`)
+        .then((lib) => setReference(lib.plainText.trim()))
+        .catch(console.error);
 
-    import(
-        `./processed/${contractFeaturesCode}/dist/schemab64.text`
-    ).then((lib) => {
-        setSchema(lib.plainText.trim());
-    });
+    import(`./processed/${contractFeaturesCode}/dist/schemab64.text`)
+        .then((lib) => setSchema(lib.plainText.trim()))
+        .catch(console.error);
 
-    return { code, reference, schema, name: `mint_wizard_${contractFeaturesCode}` };
+    return {
+        name: `mint_wizard_${contractFeaturesCode}`,
+        code,
+        reference,
+        schema,
+    };
 }
