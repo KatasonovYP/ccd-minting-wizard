6f287939907cb6df1555db480a6927e756b50d2c
Yury Katasonov <katasonov-yury@yandex.ru>
2024-05-29 15:54:56 +0300

    fix mainnet

--- client/libs/home-layers/src/entities/result-content/result-content.tsx
+++ client/libs/home-layers/src/entities/result-content/result-content.tsx
@@ -4,9 +4,8 @@ import { ConcordiumGRPCWebClient, TransactionHash } from '@concordium/web-sdk';
 import cls from './result-content.module.css';
 import { Link } from '@/shared/ui/link';
 import { Spinner } from '@/shared/ui/spinner';
-
-const explorerBaseUrl =
-    'https://testnet.ccdscan.io/?dcount=1&dentity=transaction&dhash=';
+import { useMintStore } from '@/shared/store/mint-store';
+import { MAINNET, TESTNET } from '@concordium/react-components';
 
 interface ResultContentProps {
     hash?: string;
@@ -17,25 +16,33 @@ export function ResultContent(props: ResultContentProps) {
     const { className, hash } = props;
     const [address, setAddress] = useState<string>();
     const [isLoading, setIsLoading] = useState(true);
+    const isTestNet = useMintStore((state) => state.isTestNet);
+    const net = isTestNet ? TESTNET : MAINNET;
+    const explorerBaseUrl =  net.ccdScanBaseUrl +'/?dcount=1&dentity=transaction&dhash=';
 
     useEffect(() => {
         if (!hash) {
             setAddress('no hash');
             return;
         }
-        const grpcUrl = 'https://grpc.testnet.concordium.com';
+        const grpcUrl = isTestNet
+            ? 'https://grpc.testnet.concordium.com'
+            : 'https://grpc.mainnet.concordium.software/';
         const grpcPort = 20000;
-        const client = new ConcordiumGRPCWebClient(grpcUrl, grpcPort);
+        const client = new ConcordiumGRPCWebClient(
+            grpcUrl,
+            grpcPort,
+            net.grpcOpts,
+        );
         client
-            .waitForTransactionFinalization(
-                TransactionHash.fromHexString(hash!),
-            )
+            .waitForTransactionFinalization(TransactionHash.fromHexString(hash))
             .then((result) =>
                 setAddress(
                     // @ts-expect-error contractInitialized actually exists
                     result.summary.contractInitialized.address.index.toString(),
                 ),
             )
+            .catch(console.error)
             .finally(() => setIsLoading(false));
     }, [hash]);
 
