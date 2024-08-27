ba6ec1c884358b726f432d037e7990ab8e3d009a
katasonovyp <katasonovyp.work@gmail.com>
2024-08-27 14:29:33 +0300

    revert

--- client/libs/shared/store/src/mint-store/model/default-metadata-values.ts
+++ client/libs/shared/store/src/mint-store/model/default-metadata-values.ts
@@ -6,7 +6,7 @@ export const defaultMetadataValues = {
     optionalFields: {
         symbol: undefined,
         unique: undefined,
-        decimals: 6,
+        decimals: undefined,
     },
     display: {
         display: undefined,
